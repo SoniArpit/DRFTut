bedb7505888cfdb0ee7e873be3127225a8d546af
SoniArpit <arpitsoni362@gmail.com>
2020-12-16 11:40:45 +0530

    Basic Authentication and Permission Class

---
+++ basicauthperm/fbapi/views.py
@@ -0,0 +1,16 @@
+# Basic Authentication and Permission Class
+
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    authentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated]  # access for all registered user
+    permission_classes = [IsAdminUser]  # if is_staff is true
