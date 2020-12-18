4cab45b5023a71b14a12993af8d684a046b0243c
SoniArpit <arpitsoni362@gmail.com>
2020-12-18 10:38:24 +0530

    authentication and permission in function based view

---
+++ authandperinfbv/fbapi/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.response import Response
+
+from .models import Student
+
+from .serializers import StudentSerializer
+
+from rest_framework import status
+
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+# @api_view(['GET'])  # by default GET
+# def hello_world(request):
+#     return Response({'msg': "Hello World"})
+
+# @api_view(['POST', 'GET'])
+# def hello_world(request):
+#     if request.method == 'GET':
+#         return Response({'msg': "Hello World by get request"})
+#     if request.method == 'POST':
+#         print(request.data)
+#         return Response({
+#             'msg': "Hello World by Post request",
+#             'data': request.data
+#         })
+
+
+@api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def student_api(request, pk=None):
+    if request.method == 'GET':
+        # id = request.data.get('id')
+        id = pk
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        # print(serializer.data)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Inserted'},
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'PUT':
+        # id = request.data.get('id')
+        id = pk
+        stu = Student.objects.get(pk=id)
+        serializer = StudentSerializer(stu, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Complete Data Updated'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'PATCH':
+        # id = request.data.get('id')
+        id = pk
+        stu = Student.objects.get(pk=id)
+        serializer = StudentSerializer(stu, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Partial Data Updated'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == 'DELETE':
+        # id = request.data.get('id')
+        id = pk
+        stu = Student.objects.get(pk=id)
+        stu.delete()
+        return Response({'msg': 'Data deleted'})
