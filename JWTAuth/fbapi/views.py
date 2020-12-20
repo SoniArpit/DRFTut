1ee2c42fe50fea1f6afe045b1aa2f68232a13d1d
SoniArpit <arpitsoni362@gmail.com>
2020-12-20 11:19:53 +0530

    JSON Web Token and Simple JWT in Django REST Framework

---
+++ JWTAuth/fbapi/views.py
@@ -0,0 +1,17 @@
+# JSON Web Token and Simple JWT in Django REST Framework
+
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+# from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from .customauth import CustomAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
