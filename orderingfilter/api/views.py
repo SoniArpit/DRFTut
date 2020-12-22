18fd6127ad8e3203c2d5aa5523e4c937596c8021
SoniArpit <arpitsoni362@gmail.com>
2020-12-22 11:43:06 +0530

    Ordering Filter

---
+++ orderingfilter/api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from .serializers import StudentSerializer
+from rest_framework.generics import ListAPIView
+
+from .models import Student
+
+from rest_framework.filters import OrderingFilter
+
+
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['name'
+                       ]  # if not mantioned then order by all fields appear
